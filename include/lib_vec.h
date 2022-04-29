7b30f47c3c907cf21682f133469cda645aafcd38
RubenNijhuis <contact@rubennijhuis.com>
2022-04-29 20:47:35 +0200

    ⚙️ fix: no more mallocs

--- include/lib_vec.h
+++ include/lib_vec.h
@@ -6,7 +6,7 @@
 /*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/13 16:38:43 by rubennijhui   #+#    #+#                 */
-/*   Updated: 2022/04/28 12:41:02 by rnijhuis      ########   odam.nl         */
+/*   Updated: 2022/04/29 19:31:42 by rnijhuis      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,10 @@
 
 typedef struct s_vec
 {
-	long			*values;
-	unsigned int	size;
+	int			values[3];
 }t_vec;
 
-void	set_values(t_vec *vec, long *values, unsigned int size);
-t_vec	*create_vec(long *values, unsigned int size);
+t_vec	create_vec(int values, unsigned int size);
 void	add_vec(t_vec *vec1, t_vec *vec2);
 void	subtract_vec(t_vec *vec1, t_vec *vec2);
 void	print_vec(t_vec *vec);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/13 16:38:00 by rubennijhui   #+#    #+#                 */
-/*   Updated: 2022/04/28 12:39:20 by rnijhuis      ########   odam.nl         */
+/*   Updated: 2022/04/29 19:31:30 by rnijhuis      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void	set_values(t_vec *vec, long *values, unsigned int size)
+t_vec	create_vec(int values, unsigned int size)
 {
-	unsigned int		i;
-	long	*new_values;
-	
-	new_values = malloc(sizeof(long) * size);
-	if (!new_values)
-		return ;
-	i = 0;
-	while (i < size)
-	{
-		new_values[i] = values[i];
-		i++;
-	}
-	vec->values = new_values;
-}
-
-t_vec	*create_vec(long *values, unsigned int size)
-{
-	t_vec *new_vec;
-
-	new_vec = malloc(sizeof(t_vec));
-	if (!new_vec)
-		return (NULL);
-	new_vec->size = size;
+	t_vec new_vec;
 	set_values(new_vec, values, size);
 	return (new_vec);
 }
@@ -48,10 +26,8 @@ void	add_vec(t_vec *vec1, t_vec *vec2)
 {
 	unsigned int	i;
 
-	if (vec1->size != vec2->size)
-		return ;
 	i = 0;
-	while (i < vec1->size)
+	while (i < 3)
 	{
 		vec1->values[i] += vec2->values[i];
 		i++;
@@ -62,10 +38,8 @@ void	subtract_vec(t_vec *vec1, t_vec *vec2)
 {
 	unsigned int	i;
 
-	if (vec1->size != vec2->size)
-		return ;
 	i = 0;
-	while (i < vec1->size)
+	while (i < 3)
 	{
 		vec1->values[i] -= vec2->values[i];
 		i++;
@@ -77,7 +51,7 @@ void	print_vec(t_vec *vec)
 	unsigned int	i;
 
 	i = 0;
-	while (i < vec->size)
+	while (i < 3)
 	{
 		printf("pos[%i] - val[%li]\n", i, vec->values[i]);
 		i++;
