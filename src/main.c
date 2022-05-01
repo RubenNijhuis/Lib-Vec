960ae562e1f90af816a84fd787189e3c5c7e3622
Ruben Nijhuis <contact@rubennijhuis.com>
2022-05-01 11:36:27 +0200

    Norm rul + refactor

--- src/main.c
+++ src/main.c
@@ -6,7 +6,13 @@
 /*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/13 16:38:00 by rubennijhui   #+#    #+#                 */
-/*   Updated: 2022/04/30 14:49:46 by rubennijhui   ########   odam.nl         */
+/*   Updated: 2022/04/30 18:03:21 by rubennijhui   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libvec.h"
+
+int	returnone(void)
+{
+	return (1);
+}
--- tests/src/set_value.c
+++ tests/src/set_value.c
@@ -6,42 +6,42 @@
 /*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/25 22:42:56 by rubennijhui   #+#    #+#                 */
-/*   Updated: 2022/04/28 22:07:53 by rnijhuis      ########   odam.nl         */
+/*   Updated: 2022/05/01 09:38:47 by rubennijhui   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Lib to be tested
 
 // Functions to compare original to real version
-#include "lib_vec.h"
+#include "libvec.h"
 #include <string.h>
 #include <stdio.h>
 
 // Testing lib
 #include <criterion/criterion.h>
 
-Test(addition, basic) {
-	t_vec *main_vec;
-	long values[3] = {1, 1, 1};
-	main_vec = create_vec(values, 3);
+Test(addition, basic)
+{
+	t_vec3i	*main_vec;
+	t_vec3i	*secondary_vec;
+	long	values[3] = {1, 1, 1};
+	long	values2[3] = {2, 2, 2};
 
-	t_vec *secondary_vec;
-	long values2[3] = {2, 2, 2};
+	main_vec = create_vec(values, 3);
 	secondary_vec = create_vec(values2, 3);
-	
 	add_vec(main_vec, secondary_vec);
 	cr_expect(main_vec->values[0] == 3, "Expected the new value to be 3");
 }
 
-Test(subtract, basic) {
-	t_vec *main_vec;
-	long values[3] = {1, 1, 1};
-	main_vec = create_vec(values, 3);
+Test(subtract, basic)
+{
+	t_vec3i	*main_vec;
+	t_vec3i	*secondary_vec;
+	long	values[3] = {1, 1, 1};
+	long	values2[3] = {2, 2, 2};
 
-	t_vec *secondary_vec;
-	long values2[3] = {2, 2, 2};
+	main_vec = create_vec(values, 3);
 	secondary_vec = create_vec(values2, 3);
-	
 	subtract_vec(main_vec, secondary_vec);
 	cr_expect(main_vec->values[0] == -1, "Expected the new value to be -1");
 }
